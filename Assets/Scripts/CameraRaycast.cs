191f5bace3964adeeacdc8dc1f9426bda0a7e3bf
lobinux <lobinuxsoft@gmail.com>
2022-04-07 10:48:30 -0300

    Make a camera raycast

---
+++ Assets/Scripts/CameraRaycast.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycast : MonoBehaviour
+{
+    [SerializeField] LayerMask raycastMask;
+    [SerializeField] Transform target;
+
+    RaycastHit hit;
+
+    MeshRenderer lastHit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Linecast(transform.position, target.position, out hit, raycastMask);
+
+        if(hit.transform != null && hit.transform.TryGetComponent<MeshRenderer>(out MeshRenderer mesh))
+        {
+            if(lastHit != null)
+            {
+                lastHit.enabled = lastHit != mesh;
+
+                if (lastHit != mesh) lastHit = mesh;
+            }
+            else
+            {
+                lastHit = mesh;
+            }
+        }
+        else
+        {
+            if (lastHit != null) lastHit.enabled = true;
+        }
+    }
+}
